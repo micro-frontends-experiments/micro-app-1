fae95ff1d898e20d176664a2eb472b27406c3d8d
danlaptev <danlaptev@yandex-team.ru>
2022-03-27 15:34:22 +0300

    MVP 1.0

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>App 1</h1>
-      </header>
-    </div>
-  );
+    console.log('MicroApp1 has rendered')
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1>App 1</h1>
+                </header>
+            </div>
+      );
 }
 
 export default App;
