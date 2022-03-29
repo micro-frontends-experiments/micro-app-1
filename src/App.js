a34e992c7247a3f63dc6e89e68332260e5efefe6
danlaptev <danlaptev@yandex-team.ru>
2022-03-29 22:44:00 +0300

    get title from backend

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import {useEffect, useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
     console.log('MicroApp1 has rendered')
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1>App 1</h1>
-                </header>
-            </div>
-      );
+
+    const [title, setTitle] = useState('')
+
+    useEffect(() => {
+        fetch('http://localhost:8001/micro-app-1')
+            .then(async (response) => {
+                const {title} = await response.json();
+                if (title) {
+                    setTitle(title)
+                }
+            })
+    }, [])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>{title}</h1>
+            </header>
+        </div>
+  );
 }
 
 export default App;
