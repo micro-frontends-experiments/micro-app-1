e6432acf2da628016044b8891e658c5f9402795b
danlaptev <danlaptev@yandex-team.ru>
2022-04-05 17:20:46 +0300

    auto deploying test

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1>{title}</h1>
+                <h1>{title} with Auto Deploying</h1>
             </header>
         </div>
   );
